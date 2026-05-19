bb2040867301fc6960132771ca1706a6a4e9bf6c
snehasri193 <guggilapusnehasrilatha2036.sse@saveetha.com>
2026-05-19 16:11:23 +0530

    Create Vowels.java

---
+++ Strings/Vowels.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
