2a2580019718a7cf002b01a571367db17d59c1de
snehasri193 <guggilapusnehasrilatha2036.sse@saveetha.com>
2026-05-19 16:01:25 +0530

    Add PalindromeString.java to check for palindromes

---
+++ Strings/PalindromeString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine();
+        int start = 0;
+        int end = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
