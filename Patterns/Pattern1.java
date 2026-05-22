10d81d948a1bd043adc0066bfa462560f5bda1c2
snehasri193 <guggilapusnehasrilatha2036.sse@saveetha.com>
2026-05-22 22:07:46 +0530

    Add pattern printing program in Pattern1.java

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=0;i<=n;i++){                //i=rows
+		    for(int j=0;j<=i;j++){           // j= stars
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
