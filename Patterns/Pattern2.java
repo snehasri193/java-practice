d51822ba2c96ade6e1ae7ca1acd593acb7cc0172
snehasri193 <guggilapusnehasrilatha2036.sse@saveetha.com>
2026-06-06 15:55:38 +0530

    Add Pattern2.java to print a star pattern

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++){                //i=rows
+		    for(int j=1;j<=n-i+1;j++){           // j= stars
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
